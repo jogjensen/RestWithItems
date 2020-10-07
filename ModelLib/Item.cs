0625016dc8d3741dcd0f09dae7fb2317e354f2ec
Philip Lind Jensen <jogjensen@hotmail.com>
2020-10-07 11:40:18 +0200

    Add project files.

---
+++ ModelLib/Item.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelLib
+{
+    public class Item
+    {
+        
+        #region Instance fields
+        private int _id;
+        public string _name;
+        public string _quality;
+        public double _quantity;
+        #endregion
+
+        public Item(int id, string name, string quality, double quantity)
+        {
+            _id = id;
+            _name = name;
+            _quality = quality;
+            _quantity = quantity;
+        }
+
+        public Item()
+        {
+
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Quality
+        {
+            get { return _quality; }
+            set { _quality = value; }
+        }
+
+        public double Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Quality)}: {Quality}, {nameof(Quantity)}: {Quantity}";
+        }
+    }
+}
---
+++ RestItemService/Controllers/ItemsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestItemService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        // GET: api/<ItemsController>
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
+
+        // GET api/<ItemsController>/5
+        [HttpGet("{id}")]
+        public Item Get(int id)
+        {
+            return items.Find(i=>i.Id==id);
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public void Post([FromBody] Item value)
+        {
+            items.Add(value);
+        }
+
+        // PUT api/<ItemsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Item value)
+        {
+            Item item = Get(id);
+            if (item !=null)
+            {
+                item.Id = value.Id;
+                item.Name = value.Name;
+                item.Quality = value.Quality;
+                item.Quantity = value.Quantity;
+            }
+        }
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Item item = Get(id);
+            items.Remove(item);
+        }
+
+
+        private static readonly List<Item> items = new List<Item>()
+        {
+            new Item(1,"Bread","Low",33),
+            new Item(2,"Bread","Middle",21),
+            new Item(3,"Beer","low",70.5),
+            new Item(4,"Soda","High",21.4),
+            new Item(5,"Milk","Low",55.8)
+        };
+
+    }
+
+    
+}
